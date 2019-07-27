8e727096b2313208ddc644146b08aaaeb204dcbe
Code-SY <syanina@gmail.com>
2019-07-26 20:08:27 -0700

    Version of seattle news web side scraper with mongo and cheerio

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # CheerMongoScrape
+
+## Overview
+
+This app scrapes articles from The Seattle Times website and lets users save articles and leave comments.
+
+[Heroku deploiment]()
+
+## How It Works
+
+- Click the "Scrape Articles" button to scrape articles you like.
+- Click on link to read the article then click the "Save Article" button to save article you like.
+- You can view articles you like at the "Saved Articles" page.
+- You can add comments on saved articles just click the "Add a Note" button.
+- You can post as many notes as many you would like, if you dont like some comment you can delite by clik on red " X " button.
+- You can delet saved articles by click the "Delete" button.
+
+## Technologies Used
+
+- Node.js
+- Express.js
+- Handlebars.js
+- MongoDB
+- Mongoose
+- npm packages
+    - body-parser
+    - express
+    - express-handlebars
+    - request
+    - mongoose
+    - cheerio
---
+++ server.js
@@ -0,0 +1,234 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var path = require("path");
+
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+
+var request = require("request");
+var cheerio = require("cheerio");
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+app.use(express.static("public"));
+
+var exphbs = require("express-handlebars");
+
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+    partialsDir: path.join(__dirname, "/views/layouts/partials")
+  })
+);
+
+app.set("view engine", "handlebars");
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
+
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
+
+app.get("/", function(req, res) {
+  Article.find(
+    {
+      saved: false
+    },
+    function(error, data) {
+      var hbsObject = {
+        article: data
+      };
+      console.log(hbsObject);
+      res.render("home", hbsObject);
+    }
+  );
+});
+
+app.get("/saved", function(req, res) {
+  Article.find({
+    saved: true
+  })
+    .populate("notes")
+    .exec(function(error, articles) {
+      var hbsObject = {
+        article: articles
+      };
+      res.render("saved", hbsObject);
+    });
+});
+
+app.get("/scrape", function(req, res) {
+  request("https://www.seattletimes.com/seattle-news/politics/", function(
+    error,
+    response,
+    html
+  ) {
+    var $ = cheerio.load(html);
+
+    $("article").each(function(i, element) {
+      var result = {};
+      summary = "";
+
+      summary = $(this)
+        .find("p")
+        .text();
+
+      result.title = $(this)
+        .find("h3")
+        .text();
+      result.summary = summary;
+      result.link = $(this)
+        .find("a")
+        .attr("href");
+
+      var entry = new Article(result);
+
+      entry.save(function(err, doc) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(doc);
+        }
+      });
+    });
+    res.send("Scrape Complete");
+  });
+});
+
+app.get("/articles", function(req, res) {
+  Article.find({}, function(error, doc) {
+    if (error) {
+      console.log(error);
+    } else {
+      res.json(doc);
+    }
+  });
+});
+
+app.get("/articles/:id", function(req, res) {
+  Article.findOne({
+    _id: req.params.id
+  })
+    .populate("note")
+    .exec(function(error, doc) {
+      if (error) {
+        console.log(error);
+      } else {
+        res.json(doc);
+      }
+    });
+});
+
+app.post("/articles/save/:id", function(req, res) {
+  Article.findOneAndUpdate(
+    {
+      _id: req.params.id
+    },
+    {
+      saved: true
+    }
+  ).exec(function(err, doc) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(doc);
+    }
+  });
+});
+
+app.post("/articles/delete/:id", function(req, res) {
+  Article.findOneAndUpdate(
+    {
+      _id: req.params.id
+    },
+    {
+      saved: false,
+      notes: []
+    }
+  ).exec(function(err, doc) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(doc);
+    }
+  });
+});
+
+app.post("/notes/save/:id", function(req, res) {
+  var newNote = new Note({
+    body: req.body.text,
+    article: req.params.id
+  });
+  console.log(req.body);
+  newNote.save(function(error, note) {
+    if (error) {
+      console.log(error);
+    } else {
+      Article.findOneAndUpdate(
+        {
+          _id: req.params.id
+        },
+        {
+          $push: {
+            notes: note
+          }
+        }
+      ).exec(function(err) {
+        if (err) {
+          console.log(err);
+          res.send(err);
+        } else {
+          res.send(note);
+        }
+      });
+    }
+  });
+});
+
+app.delete("/notes/delete/:note_id/:article_id", function(req, res) {
+  Note.findOneAndRemove(
+    {
+      _id: req.params.note_id
+    },
+    function(err) {
+      if (err) {
+        console.log(err);
+        res.send(err);
+      } else {
+        Article.findOneAndUpdate(
+          {
+            _id: req.params.article_id
+          },
+          {
+            $pull: {
+              notes: req.params.note_id
+            }
+          }
+        ).exec(function(err) {
+          if (err) {
+            console.log(err);
+            res.send(err);
+          } else {
+            res.send("Note Deleted");
+          }
+        });
+      }
+    }
+  );
+});
+
+app.listen(port, function() {
+  console.log("App running on port " + port);
+});
